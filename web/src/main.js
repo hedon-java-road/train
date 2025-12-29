69648dfa0a9e3cf5b098aa07e932250a3020753c
hedon954 <171725713@qq.com>
2025-12-29 23:54:37 +0800

    refactor(web): 优化 main.js 文件，增强 axios 拦截器功能
    - 更新 axios 请求拦截器，添加 token 处理逻辑
    - 修改响应拦截器，处理 401 状态码并跳转到登录页

--- web/src/main.js
+++ web/src/main.js
@@ -1,40 +1,53 @@
-import * as Icons from '@ant-design/icons-vue';
-import Antd from 'ant-design-vue';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Antd, {notification} from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
 import axios from 'axios';
-import { createApp } from 'vue';
-import App from './App.vue';
 import './assets/js/enums';
-import router from './router';
-import store from './store';
 
 const app = createApp(App);
-app.use(Antd).use(store).use(router).mount('#app')
+app.use(Antd).use(store).use(router).mount('#app');
 
-// 全局使用图片
+// 全局使用图标
 const icons = Icons;
 for (const i in icons) {
-    app.component(i, icons[i]);
+  app.component(i, icons[i]);
 }
 
-// axios 拦截器
-axios.interceptors.request.use(config => {
-    console.log('请求参数: ', config);
-    return config;
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+  console.log('请求参数：', config);
+  const _token = store.state.member.token;
+  if (_token) {
+    config.headers.token = _token;
+    console.log("请求headers增加token:", _token);
+  }
+  return config;
 }, error => {
-    return Promise.reject(error);
-});
-axios.interceptors.response.use(response => {
-  console.log('响应参数: ', response);
-  return response;
-}, error => {
-  console.log('响应错误: ', error);
   return Promise.reject(error);
 });
-
-// 输出配置信息
-console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
-console.log('process.env.VUE_APP_SERVER: ', process.env.VUE_APP_SERVER);
-
-// 配置 axios 默认服务器请求前缀
+axios.interceptors.response.use(function (response) {
+  console.log('返回结果：', response);
+  return response;
+}, error => {
+  console.log('返回错误：', error);
+  const response = error.response;
+  const status = response.status;
+  if (status === 401) {
+    // 判断状态码是401 跳转到登录页
+    console.log("未登录或登录超时，跳到登录页");
+    store.commit("setMember", {});
+    notification.error({ description: "未登录或登录超时" });
+    router.push('/login');
+  }
+  return Promise.reject(error);
+});
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+console.log('环境：', process.env.NODE_ENV);
+console.log('服务端：', process.env.VUE_APP_SERVER);
+
