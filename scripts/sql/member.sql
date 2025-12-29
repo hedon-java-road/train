6ad1ed67da07e8868677131c06e3ebd590029391
hedon954 <171725713@qq.com>
2025-12-30 03:11:41 +0800

    feat(member): 添加乘客管理功能及相关配置
    - 更新 pom.xml，修改 MyBatis Generator 配置文件路径
    - 新增乘客相关的 XML 配置文件、Java 实体类、Mapper 接口及示例类
    - 在 SQL 脚本中添加乘客表的创建语句
    - 实现乘客的 CRUD 操作接口

--- scripts/sql/member.sql
+++ scripts/sql/member.sql
@@ -9,3 +9,17 @@ CREATE TABLE member (
     `id` BIGINT PRIMARY KEY COMMENT 'id',
     `mobile` VARCHAR(11) UNIQUE NOT NULL COMMENT '手机号'
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '会员表';
+
+DROP TABLE IF EXISTS `passenger`;
+
+CREATE TABLE passenger (
+    `id` BIGINT COMMENT 'id',
+    `member_id` BIGINT NOT NULL COMMENT '会员id',
+    `name` VARCHAR(20) NOT NULL COMMENT '姓名',
+    `id_card` VARCHAR(18) NOT NULL COMMENT '身份证',
+    `type` VARCHAR(1) NOT NULL COMMENT '旅客类型|枚举[PassengerTypeEnum]',
+    `create_time` DATETIME(3) NOT NULL COMMENT '创建时间',
+    `update_time` DATETIME(3) NOT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`),
+    INDEX `idx_member_id` (`member_id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '乘车人';
