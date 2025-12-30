427812c6d21aa2b99acbdd25df585eb9c911c727
hedon954 <171725713@qq.com>
2025-12-30 14:30:02 +0800

    feat(member): 添加 @JsonSerialize 注解以解决 long 类型精度丢失问题

---
+++ common/src/main/java/com/hedon/train/common/config/JacksonConfig.java
@@ -0,0 +1,18 @@
+// package com.hedon.train.common.config;
+
+/**
+ * 统一注解，解决前后端交互 long 类型精度丢失的问题
+ */
+// @Configuration
+// public class JacksonConfig {
+
+// @Bean
+// public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder)
+// {
+// ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+// SimpleModule simpleModule = new SimpleModule();
+// simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+// objectMapper.registerModule(simpleModule);
+// return objectMapper;
+// }
+// }
--- member/src/main/java/com/hedon/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/com/hedon/train/member/resp/PassengerQueryResp.java
@@ -3,10 +3,15 @@ package com.hedon.train.member.resp;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 public class PassengerQueryResp {
+
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long memberId;
 
     private String name;
